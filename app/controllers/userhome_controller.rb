fc355149b74b5f6d010ad5dfa58e810272eceae7
rayraykay <raymond.kim243@outlook.com>
2017-06-19 19:49:12 -0400

    extra formatting in code

--- app/controllers/userhome_controller.rb
+++ app/controllers/userhome_controller.rb
@@ -40,26 +40,26 @@ class UserhomeController < ApplicationController
     end
 
     private
-    def levenshtein(first, second)
-        matrix = [(0..first.length).to_a]
-        (1..second.length).each do |j|
-            matrix << [j] + [0] * (first.length)
-        end
+        def levenshtein(first, second)
+            matrix = [(0..first.length).to_a]
+            (1..second.length).each do |j|
+                matrix << [j] + [0] * (first.length)
+            end
 
-        (1..second.length).each do |i|
-            (1..first.length).each do |j|
-                if first[j-1] == second[i-1]
-                    matrix[i][j] = matrix[i-1][j-1]
-                else
-                    matrix[i][j] = [
-                      matrix[i-1][j],
-                      matrix[i][j-1],
-                      matrix[i-1][j-1],
-                    ].min + 1
+            (1..second.length).each do |i|
+                (1..first.length).each do |j|
+                    if first[j-1] == second[i-1]
+                        matrix[i][j] = matrix[i-1][j-1]
+                    else
+                        matrix[i][j] = [
+                          matrix[i-1][j],
+                          matrix[i][j-1],
+                          matrix[i-1][j-1],
+                        ].min + 1
+                    end
                 end
             end
-        end
 
-        return matrix.last.last
-    end
+            return matrix.last.last
+        end
 end
